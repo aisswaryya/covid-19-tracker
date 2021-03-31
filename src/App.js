70ca5cdd5095a2d31574776b941d709b72b60a8f
Aishwarya <39556471+aisswaryya@users.noreply.github.com>
2021-03-30 23:50:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import { MenuItem, FormControl, Select } from '@material-ui/core'
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from '@material-ui/core'
 import InfoBox from './InfoBox'
 import Map from './Map'
+import Table from './Table'
+import { sortData } from './util'
+import LineGraph from './LineGraph'
 import './App.css'
 
 function App() {
   const [countries, setCountries] = useState([])
   const [country, setCountry] = useState('worldwide') //default value
+  const [countryInfo, setCountryInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+
+  //populate country table for worldwide input
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data)
+      })
+  }, [])
+
+  //populate country table for specific country
   useEffect(() => {
     const getCountries = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
@@ -17,6 +39,8 @@ function App() {
             value: country.countryInfo.iso2,
           }))
 
+          const sortedData = sortData(data)
+          setTableData(sortedData)
           setCountries(countries)
         })
     }
@@ -24,9 +48,20 @@ function App() {
     getCountries()
   }, [])
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value
     setCountry(countryCode)
+
+    const url =
+      country === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/cdiov-19/countries/${countryCode}`
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data)
+      })
   }
   return (
     <div className='app'>
@@ -48,12 +83,34 @@ function App() {
         </div>
 
         <div className='app__stats'>
-          <InfoBox title='Coronovirus Cases' cases={100} total={2000} />
-          <InfoBox title='Recovered' cases={399} total={199} />
-          <InfoBox title='Deaths' cases={400} total={198} />
+          <InfoBox
+            title='Coronovirus Cases'
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title='Recovered'
+            cases={countryInfo.recovered}
+            total={countryInfo.todayRecovered}
+          />
+          <InfoBox
+            title='Deaths'
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
         <Map />
       </div>
+      <Card className='app__right'>
+        <CardContent>
+          {/* table */}
+          <h3>Live cases by Country</h3>
+          <Table countries={tableData} />
+          {/* graph */}
+          <h3>Worldwide new Cases</h3>
+          <LineGraph></LineGraph>
+        </CardContent>
+      </Card>
     </div>
   )
 }
